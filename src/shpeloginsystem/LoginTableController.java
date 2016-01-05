b35a1ad3e11e75a706a0188761a4536c6d55f8dc
Jose Roque <jose.roque@mavs.uta.edu>
2016-01-05 09:27:19 -0600

    Added Login Table for user to check their name off and sign in conveniently

---
+++ src/shpeloginsystem/LoginTableController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shpeloginsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author JoseRoque
+ */
+public class LoginTableController extends TableView implements Initializable{
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
